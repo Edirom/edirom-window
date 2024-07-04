861337981098fbaa111604a9c68b0eb317f1e8e7
Hizkiel <hizclick@gmail.com>
2024-07-04 15:52:37 +0200

    Arranging windows
    Arranging windows in vertical or horizontal grid.

--- edirom-windows.js
+++ edirom-windows.js
@@ -1,20 +1,15 @@
 class EdiromWindows extends HTMLElement {
-    
     constructor() {
-
         super();
 
         // Define the default properties
-        this.windows = [ ];
+        this.windows = [];
         this.windowsDefaults = {
             border: "0.3em",
             background: "#ccc"
         }
-
-        
         // Create shadow DOM
         this.attachShadow({ mode: 'open' });
-
 
         // Load the winbox library
         const winboxScript = document.createElement('script');
@@ -22,7 +17,7 @@ class EdiromWindows extends HTMLElement {
         winboxScript.defer = true;
         this.shadowRoot.appendChild(winboxScript);
 
-        // add the winbox css
+        // Add the winbox css
         const winboxCss = document.createElement('link');
         winboxCss.rel = "stylesheet";
         winboxCss.href = "https://rawcdn.githack.com/nextapps-de/winbox/0.2.82/dist/css/winbox.min.css";
@@ -31,120 +26,170 @@ class EdiromWindows extends HTMLElement {
 
         // When the winbox library is loaded
         winboxScript.onload = () => {
-
-            // loop through the windows array
-            for(var i=0; i<this.windows.length; i++){
-
-                // add the default properties
+            // Loop through the windows array
+            for (var i = 0; i < this.windows.length; i++) {
+                // Add the default properties
                 for (var key in this.windowsDefaults) {
                     if (!this.windows[i].hasOwnProperty(key)) {
                         this.windows[i][key] = this.windowsDefaults[key];
                     }
                 }
 
-                // add root key
+                // Add root key
                 this.windows[i].root = this.shadowRoot;
 
                 // Create the window
-                new WinBox( this.windows[i] );
+                new WinBox(this.windows[i]);
             }
 
-        }
 
-
+        };
     }
 
-    // register the attributes to be observed
+    // Register the attributes to be observed
     static get observedAttributes() {
-        return ['set', 'add', 'remove'];
-    }   
-    
-
-    // connected callback
-    connectedCallback() {
-
-
-
+        return ['set', 'add', 'remove', 'update', 'arrange'];
     }
 
-
-    // attribute change
+    // Attribute change
     attributeChangedCallback(property, oldValue, newValue) {
-
         // Custom event for the attribute change
-        const event = new CustomEvent('communicate-'+property+'-update', {
+        const event = new CustomEvent('communicate-' + property + '-update', {
             detail: { [property]: newValue },
             bubbles: true
         });
         this.dispatchEvent(event);
 
         // Check the property
-        switch(property) {
+        switch (property) {
             case "set":
-                //remove all winbox windows from DOM
+                // Remove all winbox windows from DOM
                 this.shadowRoot.querySelectorAll('.winbox').forEach(e => e.remove());
 
-                // add the new windows
-                if (newValue != ""){
+                // Add the new windows
+                if (newValue != "") {
                     this.add(JSON.parse(newValue));
                 }
                 break;
             case "add":
-                if (newValue != ""){
+                if (newValue != "") {
                     this.add(JSON.parse(newValue));
                 }
                 break;
             case "remove":
-                if (newValue != ""){
+                if (newValue != "") {
                     this.remove(newValue);
+                }
+                break;
+            case "update":
+                if (newValue != "") {
+                    this.update(JSON.parse(newValue));
+                }
+                break;
+            case "arrange":
+                if (newValue != "") {
+                    this.arrange(newValue);
                 }
                 break;
             default:
                 break;
         }
-        
     }
 
-    
-    // add a window
-    add(windows){
-
-        // loop through the windows array
-        for(var i=0; i<windows.length; i++){
-
-            // add the default properties
+    // Add a window
+    add(windows) {
+        // Loop through the windows array
+        for (var i = 0; i < windows.length; i++) {
+            // Add the default properties
             for (var key in this.windowsDefaults) {
                 if (!windows[i].hasOwnProperty(key)) {
                     windows[i][key] = this.windowsDefaults[key];
                 }
             }
 
-            // add the window to the global windows property
+            // Add the window to the global windows property
             this.windows.push(windows[i]);
 
-            // add root key
+            // Add root key
             windows[i].root = this.shadowRoot;
 
             // Create the window
-            const wb = new WinBox( windows[i] );
-
+            new WinBox(windows[i]);
         }
     }
 
-    // remove a window
-    remove(id){
-
-        // remove the window from the global windows property
-        this.windows = this.windows.filter(function( obj ) {
+    // Remove a window
+    remove(id) {
+        // Remove the window from the global windows property
+        this.windows = this.windows.filter(function (obj) {
             return obj.id !== id;
         });
 
-        // remove the window from the DOM
-        this.shadowRoot.getElementById(id).remove();
-
+        // Remove the window from the DOM
+        const element = this.shadowRoot.getElementById(id);
+        if (element) {
+            element.remove();
+        }   
     }
 
+    // Update windows
+    update(windows) {
+
+        // Case 1: update all the windows with the same values eg. arranging the windows,
+        // windows =  [{"height": "95%"}]
+        // Case 2: Update each window with different values
+
+
+        // Loop through the json string sent to update function
+        for (var i = 0; i < windows.length; i++) {
+            // Loop through all the existing editom windows
+            for (var j = 0; j < this.windows.length; j++) {
+                // Check if the id of the input json string is the same as this.windows
+                if (windows[i]["id"] == this.windows[j]["id"]) {
+                    for (var key in windows[i]) {
+                        this.windows[j][key] = windows[i][key];
+                    }
+                }
+            }
+        }
+            
+    }
+
+    arrange(type){
+            // Log all top-level div elements inside the shadow root
+            var topLevelDivs = Array.from(this.shadowRoot.querySelectorAll('.winbox'));
+            var screenWidth = screen.width;
+            var screenHeight = screen.height;
+
+
+            var contentWidth = screenWidth * 0.8
+            var contentHeight = screenHeight * 0.85
+            var gridWidth = contentWidth / topLevelDivs.length
+            var gridHeight = contentHeight / topLevelDivs.length
+
+            topLevelDivs.forEach((div, index) => {
+                if(type == "vertical") {
+                    div.style.width = gridWidth+"px" ;
+                    div.style.left = index * gridWidth + "px"
+                    div.style.top = 0
+                    div.style.height = "95%"
+                } if(type == "horizontal") {
+                    div.style.height = gridHeight+"px"
+                    div.style.top = index * gridHeight + "px"
+                    div.style.left = 0
+                    div.style.width = "80%" ;
+                }
+
+            });   
+    }
+
+
+    // Listen for communicate-update-update event and handle updates
+    connectedCallback() {
+
+    }
 }
 
 // Define the custom element
 customElements.define('edirom-windows', EdiromWindows);
+
